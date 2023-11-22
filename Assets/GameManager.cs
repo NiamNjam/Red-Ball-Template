22ef7218442d96757eeca1a8936eadfe03a094fb
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2023-11-22 20:11:18 +0200

    Otherlevels

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,13 +9,20 @@ public class GameManager : MonoBehaviour
     public int currentlevel;
     public List<string> levels;
     public static GameManager instance;
-    public AudioSource WinS;
-    public AudioClip WinC;
+    AudioSource source;
+    public AudioClip WinSound;
+    public AudioClip LoseSound;
+    public AudioClip gameOverSound;
+    //public AudioClip backgroundSound;
+
+    public Transform transition;
+    Vector3 targetScale;
 
     //singleton
 
-    private void Start()
+    void Start()
     {
+        source = GetComponent<AudioSource>();
         if (instance == null)
         {
             instance = this;
@@ -27,34 +34,47 @@ public class GameManager : MonoBehaviour
             Destroy(this);
         }
     }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.MoveTowards(transition.localScale, targetScale, 60 * Time.deltaTime);
+    }
     public void Win()
     {
         print("touch");
-        WinS.clip = WinC;
-        WinS.Play();
+        //source.clip = WinSound;
+        //source.Play(); //jei garsas pastovus
+        source.PlayOneShot(WinSound); //viena karta pagroja
         currentlevel++;
         Invoke("LoadScene", 1f);
+        targetScale = Vector3.one * 30;
         //SceneManager.LoadScene(levels[currentlevel]);
     }
 
+
+
     void LoadScene()
     {
+        targetScale = Vector3.zero;
         SceneManager.LoadScene(levels[currentlevel]);
     }
 
     public void Lose()
     {
+        targetScale = Vector3.one * 30;
         hp--;
         if (hp > 0)
         {
             //SceneManager.LoadScene(levels[currentlevel]);
             Invoke("LoadScene", 1f);
+            source.PlayOneShot(LoseSound);
         }
         else
         {
             //SceneManager.LoadScene(levels[0]);
             currentlevel = 0;
             Invoke("LoadScene", 1f);
+            source.PlayOneShot(gameOverSound);
         }
     }
 }
